3e33283709351bf99b93ed43f8238a792d4118d4
hmtheboy154 <buingoc67@gmail.com>
2024-01-11 10:01:02 -0500

    WIP test: Intel USB HAL
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- treble.mk
+++ treble.mk
@@ -87,8 +87,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-impl \
-    android.hardware.usb@1.0-service \
+    android.hardware.usb@1.1-service.intel \
     com.android.future.usb.accessory
 
 # Drm HAL
